557217ce9973035eb5290718760d0a6ed76a7505
lvinist <alvin.geomatics@gmail.com>
2022-04-09 16:19:30 +0700

    Initial commit and BaseUI

---
+++ app/src/main/java/com/alph/storyapp/ui/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.alph.storyapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.alph.storyapp.R
+
+class SplashScreen : AppCompatActivity() {
+
+    companion object {
+        private const val EXTRA_DURATION = 3000L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        getSupportActionBar()?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }, EXTRA_DURATION)
+    }
+}
---
+++ app/src/main/java/com/alph/storyapp/ui/StoryDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.alph.storyapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alph.storyapp.R
+
+class StoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_detail)
+    }
+}
